781667fadee65048ad24c2e62666d6e0fc7102ad
Saher Muhamed <81532683+SaherMuhamed@users.noreply.github.com>
2023-09-20 21:27:16 +0300

    Add files via upload

---
+++ directory_crawler.py
@@ -0,0 +1,47 @@
+import sys
+import urllib3
+import requests
+from art import directory_art
+from argparse import ArgumentParser
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/102.0.0.0 Safari/537.36'}  # It will send the request like browser
+
+
+def args():
+    parser = ArgumentParser()
+    parser.add_argument("-u", "--url", dest="target_url", help="Enter URL you want to test. Example: -u example.com")
+    parser.add_argument("-w", "--wordlist", dest="wordlist", help="Choose the wordlist file. Example: -w common.txt")
+    options = parser.parse_args()
+    if not options.target_url:
+        parser.error("[-] Please specify website url, or type it correctly, ex: -u example.com")
+    elif not options.wordlist:
+        parser.error("[-] Please specify a wordlist file, or type it correctly, ex: -w common.txt")
+    return options
+
+
+def request(website_url):
+    try:
+        return requests.get(url="http://" + website_url, headers=HEADERS)
+    except requests.exceptions.ConnectionError:
+        pass
+
+
+def main():
+    print(directory_art)
+    try:
+        with open(file=args().wordlist, mode="r") as file:
+            for line in file:
+                test_url = args().target_url + "/" + line.strip()
+                response = request(website_url=test_url)
+                if response:
+                    print("[+] Endpoint Discovered ==> http://" + test_url + " | Status Code " + str(response.status_code) + " | TTL " + str(round(response.elapsed.microseconds * 0.001, 2)) + " ms")
+                sys.stdout.flush()
+    except KeyboardInterrupt:
+        print("\n[*] Detected 'ctrl + c' pressed, program terminated.")
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
